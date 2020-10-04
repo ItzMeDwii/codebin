13b5dbd8aead15eec714b50f00e1905a3ee1fdf9
Hazmi35 <contact@hzmi.xyz>
2020-10-04 09:14:51 +0700

    chore: initial commit

---
+++ app/site/.eslintrc.js
@@ -0,0 +1,23 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require("path");
+
+module.exports = {
+    env: {
+        node: true
+    },
+    extends: [
+        "plugin:vue/essential",
+        "@vue/typescript/recommended",
+        "@hazmi35/eslint-config/typescript"
+    ],
+    parser: "vue-eslint-parser",
+    parserOptions: {
+        project: path.join(__dirname, "tsconfig.eslint.json"),
+        ecmaVersion: 2020
+    },
+    root: false,
+    rules: {
+        "no-console": "off",
+        "no-debugger": "off"
+    }
+};
---
+++ app/site/vue.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    runtimeCompiler: true,
+    productionSourceMap: true,
+    configureWebpack: {
+        devtool: "nosources-source-map"
+    },
+    pluginOptions: {
+        i18n: {
+            locale: "en",
+            fallbackLocale: "en",
+            localeDir: "locales",
+            enableInSFC: false
+        },
+        pwa: {
+            appleMobileWebAppCapable: "yes",
+            appleMobileWebAppStatusBarStyle: "blue",
+            msTileColor: "#212121",
+            name: "codebin",
+            themeColor: "#212121",
+            workboxOptions: {
+                skipWaiting: true,
+                clientsClaim: true
+            }
+        }
+    }
+};
